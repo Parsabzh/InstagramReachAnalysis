b092388e96a811c2ec3bd0605a5c54f2d30f01c3
parsabzh <parsabzh@gmail.com>
2024-02-09 15:04:09 +0100

    drop null

--- InstaAnalysis/main.py
+++ InstaAnalysis/main.py
@@ -7,5 +7,11 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import PassiveAggressiveRegressor
 
+# load dataset
 data = pd.read_csv("Instagram.csv", encoding="latin1")
 print(data.head())
+
+# check and drop null
+print(data.isnull().sum())
+data=data.dropna()
+
